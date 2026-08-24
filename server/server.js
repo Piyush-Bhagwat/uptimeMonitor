451cef6f5f73d72330a4ff20aafcf41b1089ff15
piyushBhagwat <piyushbhagwat5@gmail.com>
2026-08-24 19:14:07 +0530

    refactor: update server port configuration to use environment variable

--- server/server.js
+++ server/server.js
@@ -4,8 +4,10 @@ import { app } from "./src/app.js";
 import { connectDB } from "./src/config/mongoose.config.js";
 import redisClient from "./src/config/redis.config.js";
 
-app.listen(3030, "0.0.0.0", async () => {
-    console.log("Server started on port 3030");
+const PORT = process.env.PORT || 3030;
+
+app.listen(PORT, "0.0.0.0", async () => {
+    console.log(`Server started on port ${PORT}`);
 
     await connectDB();
     await redisClient.connect();
